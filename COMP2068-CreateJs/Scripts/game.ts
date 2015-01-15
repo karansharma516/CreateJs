f03b5bb3bc2a86b8b3e5ae6ff88613445d0855db
karan sharma <karan.sharma516@yahoo.com>
2015-01-15 12:50:49 -0800

    added files

---
+++ COMP2068-CreateJs/Scripts/game.ts
@@ -0,0 +1,24 @@
+﻿var canvas;
+var stage: createjs.Stage;
+var helloText: createjs.Text;
+
+function init() {
+
+    canas = document.getElementById("canvas");
+    stage = new cretaejs.Stage(canvas);
+    createjs.Ticker.setFPS(60);
+    createjs.Ticker.addEventListener("tick", gameLoop);
+    main();
+}
+
+funciton gameLoop() {
+    helloText.x++;
+    helloText.y++;
+
+    stage.update();
+}
+
+function main() {
+    var helloText = new createjs.Text("Hello World", "40px consoles", "#000000");
+    stage.addchild(helloText);
+}
